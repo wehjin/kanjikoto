09b2705bef3ec35c77ac031e31eee24a9fa5f4b9
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2025-12-04 10:16:31 -0800

    Updates the main navigation.

--- src/views/navbar.rs
+++ src/views/navbar.rs
@@ -2,31 +2,30 @@ use crate::Route;
 use dioxus::prelude::*;
 
 const NAVBAR_CSS: Asset = asset!("/assets/styling/navbar.css");
+const LOGO_IMG: Asset = asset!("/assets/PlaygroundImage4.jpg");
 
 /// The Navbar component that will be rendered on all pages of our app since every page is under the layout.
 #[component]
 pub fn Navbar() -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: NAVBAR_CSS }
-
-        div {
-            id: "navbar",
-            Link {
-                to: Route::Home {},
-                "Home"
+        nav { class: "navbar", role: "navigation", aria_label: "main navigation",
+            div { class: "navbar-brand",
+                Link { class: "navbar-item",
+                    to: Route::Home {},
+                    img { src: LOGO_IMG, }
+                }
             }
-            Link {
-                to: Route::Blog { id: 1 },
-                "Blog"
-            }
-            Link {
-                to: Route::Start {  },
-                "Start"
+            div { class: "navbar-menu",
+                div { class: "navbar-start",
+                    Link { class: "navbar-item", to: Route::Home {}, "Home" }
+                    Link { class: "navbar-item", to: Route::Blog { id: 1 }, "Blog" }
+                }
+                div { class: "navbar-end",
+                    Link { class: "navbar-item", to: Route::Start {}, "Settings" }
+                }
             }
         }
-
-        // The `Outlet` component is used to render the next component inside the layout. In this case, it will render either
-        // the [`Home`] or [`Blog`] component depending on the current route.
         Outlet::<Route> {}
     }
 }
