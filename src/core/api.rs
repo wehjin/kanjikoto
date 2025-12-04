e32b7d886cc423ba8ad819449d6cf37759f4a83b
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2025-12-04 09:28:17 -0800

    Pulls drills from api.

--- src/core/api.rs
+++ src/core/api.rs
@@ -3,13 +3,29 @@ use crate::core::drill_point::DrillPoint;
 const API_URL: &str = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQjXD1Z1nrpTS60VhvlyI3Gha7bS-XP1r_nv3ITYbw4JBL-FA8SB6irRsVHhlEje5ZZT_H8uwFuRGgw/pub?gid=0&single=true&output=csv";
 
 pub async fn get_drills() -> Vec<DrillPoint> {
-    let drills = vec![
-        DrillPoint::new(1, "始（はじ）まり", "the beginning"),
-        DrillPoint::new(
-            1,
-            "幸（こう）か（）不（ふ）幸（こう）か",
-            "for better or worse, lucky or unlucky",
-        ),
-    ];
+    let csv = reqwest::get(API_URL).await.unwrap().text().await.unwrap();
+    let rows = parse_rows(csv);
+    let drills = rows
+        .into_iter()
+        .map(|row| DrillPoint::new(row.chapter, row.word, row.meaning))
+        .collect::<Vec<_>>();
     drills
+}
+
+fn parse_rows(csv: String) -> Vec<CsvRow> {
+    let mut rows = vec![];
+    let mut reader = csv::Reader::from_reader(csv.as_bytes());
+    for result in reader.deserialize() {
+        let row: CsvRow = result.unwrap();
+        rows.push(row);
+    }
+    rows
+}
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct CsvRow {
+    chapter: usize,
+    word: String,
+    meaning: String,
 }
